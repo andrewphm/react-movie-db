bb90d35a99bcdf9fe72ea509887a7846c48156d9
andrewphm <andrpham@gmail.com>
2021-07-15 14:44:44 -0400

    Create hero image component with styled component. Map it to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 // Components
+import HeroImage from './HeroImage'
 
 // Custom Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -14,7 +15,17 @@ const Home = () => {
     const { state, loading, error } = useHomeFetch();
     console.log(state);
 
-    return <div>Home Page</div>
+    return (
+     <>
+        {state.results[0] ? (
+        <HeroImage 
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+        />
+       ) : null}
+     </> 
+    )
 }
 
 export default Home;
