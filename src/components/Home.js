fe8cf7f57f35cf23edd5a9e6cec61ab57e8a8c01
andrewphm <andrpham@gmail.com>
2021-07-15 13:49:51 -0400

    set up useState hooks for error and loading in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// API
+import API from '../API'
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
@@ -14,10 +16,25 @@ import NoImage from '../images/no_image.jpg'
 const Home = () => {
     // State to hold all the movies
     const [state, setState] = useState();
-
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            console.log(movies);
+        } catch (error) {
+            setError(true);
+        }
+    } 
+
+    // Initial render
+    useEffect(() => {
+        fetchMovies(1)
+    }, [])
 
     return <div>Home Page</div>
 }
