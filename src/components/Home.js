4d00bb01ef0bc02ae33ad9cf6423dd5f44747454
andrewphm <andrpham@gmail.com>
2021-07-15 14:43:27 -0400

    create custom hook for fetching movies.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,17 @@
-import React from 'react';
-// API
-import API from '../API'
+import React, { useState, useEffect } from 'react';
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 // Components
 
-// Hook
-import { useState, useEffect } from 'react';
-
+// Custom Hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
-    // State to hold all the movies
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-
-            setState(prev => ({
-                ...movies,
-                results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }));
-        } catch (error) {
-            setError(true);
-        }
-
-        setLoading(false);
-    } 
-
-    // Initial render
-    useEffect(() => {
-        fetchMovies(1)
-    }, [])
-
+    const { state, loading, error } = useHomeFetch();
     console.log(state);
 
     return <div>Home Page</div>
