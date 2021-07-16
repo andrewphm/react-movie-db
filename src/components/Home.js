fceb048e991d5f95aa63c57b3d8ff9643530681f
andrewphm <andrpham@gmail.com>
2021-07-15 21:02:58 -0400

    Create Thumbnail component with styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 // Components
 import HeroImage from './HeroImage'
 import Grid from './Grid'
+import Thumb from './Thumb'
 
 // Custom Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -30,9 +31,16 @@ const Home = () => {
 
        <Grid header='Popular Movies'>
            {state.results.map(movie => (
-                <div key={movie.id}>
-                {movie.title}
-                </div>
+                <Thumb
+                    key={movie.id}
+                    clickable
+                    image={
+                        movie.poster_path ?
+                        IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                        : NoImage
+                    } 
+                    movieId={movie.id}
+                />
            ))}
        </Grid>
      </> 
