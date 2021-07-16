4eade072bc35d043096e3633bae86ed8930e292c
andrewphm <andrpham@gmail.com>
2021-07-16 10:02:36 -0400

    Import and create scaffoling for Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
-const Movie = () => <div>Movie</div>
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+// Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+// Hook
+
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+
+    return (
+        <>
+            <div>Movie</div>
+        </>
+    )
+}
 
 
 export default Movie;
