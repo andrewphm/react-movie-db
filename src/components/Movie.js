9deb3b70ee831815d2a57323a44d51a58011c6a7
andrewphm <andrpham@gmail.com>
2021-07-16 10:37:16 -0400

    Create breadcrumb component and map to Movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import { useParams } from 'react-router-dom';
+
 
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
@@ -7,17 +8,26 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
 
 // Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
 
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
+    // Grab the movieId using useParams hook
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId)
+    
+    if (loading) return <Spinner />
+    if (error) return <div>Something went wrong... </div>;
 
     return (
         <>
-            <div>Movie</div>
+            <BreadCrumb movieTitle={movie.original_title}/>
         </>
     )
 }
---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+// API
+import API from '../API';
+
+export const useMovieFetch = movieId => {
+    const [state, setState] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            try {
+                setLoading(true);
+                setError(false);
+
+                const movie = await API.fetchMovie(movieId);
+                const credits = await API.fetchCredits(movieId);
+                // Get directors only
+                const directors = credits.crew.filter(
+                    member => member.job === 'Director'
+                )
+
+                setState({
+                    ...movie,
+                    actors: credits.cast,
+                    directors
+                })
+
+                setLoading(false);
+            } catch (error) {
+                setError(true);
+            }
+        };
+
+        fetchMovie();
+    }, [movieId])
+
+    return { state, loading, error };
+}
