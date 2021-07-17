05a756a6487638e65a6c341a180f7f22fce3429a
andrewphm <andrpham@gmail.com>
2021-07-17 15:14:21 -0400

    Add Actor component to the Movie page component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -11,6 +11,7 @@ import Spinner from './Spinner';
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
@@ -36,6 +37,19 @@ const Movie = () => {
                 budget={movie.budget} 
                 revenue={movie.revenue}
             />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
         </>
     )
 }
