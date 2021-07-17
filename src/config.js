f12883800aa9cb2c492f9524f2868da73b1b77b5
andrewphm <andrpham@gmail.com>
2021-07-17 16:22:13 -0400

    update config api settings

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '2a657d0598d8d52dc20710873772c87d';
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
