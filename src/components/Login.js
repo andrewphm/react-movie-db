ddf0a34f83ffb6976118e380c8e2ccfc6f31461c
andrewphm <andrpham@gmail.com>
2021-11-09 10:11:17 -0500

    Create login function with styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,26 +11,58 @@ import { Wrapper } from './Login.styles';
 // Context
 import { Context } from '../context';
 
+const initialState = { username: '', password: '' };
+
 const Login = () => {
-  const handleSubmit = () => {};
-  const handleInput = (e) => {};
+  const [login, setLogin] = useState(initialState);
+  const [error, setError] = useState(false);
+
+  const [_user, setUser] = useContext(Context);
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    setError(false);
+    const { username, password } = login;
+    try {
+      const requestToken = await API.getRequestToken();
+      const sessionId = await API.authenticate(
+        requestToken,
+        username,
+        password
+      );
+
+      console.log(sessionId);
+      setUser({ sessionId: sessionId.session_id, username });
+
+      navigate('/react-movie-db');
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  const handleInput = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
 
   return (
     <Wrapper>
+      {error && <div className="error">There was an error!</div>}
       <label htmlFor="username">Username: </label>
       <input
         type="text"
-        value="state value"
+        value={login.username}
         name="username"
         onChange={handleInput}
+        placeholder="username"
         required
       />
       <label htmlFor="password">Password: </label>
       <input
         type="password"
-        value="state value"
+        value={login.password}
         name="password"
         onChange={handleInput}
+        placeholder="password"
         required
       />
       <Button text="Login" callback={handleSubmit} />
--- src/components/Login.styles.js
+++ src/components/Login.styles.js
@@ -1,3 +1,37 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  max-width: 320px;
+  padding: 20px;
+  color: var(--darkGrey);
+  margin: 5rem auto;
+  background-color: lightgrey;
+  border-radius: 5px;
+  box-shadow: 0 1px 4px 2px rgba(0, 0, 0, 0.33);
+
+  input {
+    width: 100%auto;
+    height: 30px;
+    border: 1px solid var(--darkGrey);
+    border-radius: 20px;
+    margin: 10px 0;
+  }
+
+  input[type='text'],
+  input[type='password'] {
+    padding-left: 8px;
+  }
+
+  label {
+    font-weight: 900;
+  }
+
+  .error {
+    color: red;
+  }
+`;
